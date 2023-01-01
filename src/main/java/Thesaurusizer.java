35d11badad1dbbc35194d3d5b349bc6c5a038170
jbinde02 <jacobbinder00@gmail.com>
2023-01-01 01:37:09 -0500

    Added comments

--- src/main/java/Thesaurusizer.java
+++ src/main/java/Thesaurusizer.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+/*
+Main class. Contains UI and controller code.
+ */
 public class Thesaurusizer {
     private JFrame frame;
     private JPanel mainPanel;
@@ -23,15 +26,18 @@ public class Thesaurusizer {
     private ThesaurusMap thesaurusMap;
     private Thesaurusizer(){
         // <Swing>
+        // <Frame>
         frame = new JFrame("Thesaurusizer");
         frame.setSize(1000,800);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        // <Main Panel>
 
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         frame.add(mainPanel);
+        // <Input Text Area and Pane>
 
         inputArea = new JTextArea("Insert text...");
         inputArea.setLineWrap(true);
@@ -44,12 +50,14 @@ public class Thesaurusizer {
         constraints.gridy = 0;
         mainPanel.add(inputPane, constraints);
 
+        // <Submit Button>
         submitButton = new JButton("Submit");
         constraints.anchor = GridBagConstraints.LINE_START;
         constraints.gridx = 0;
         constraints.gridy = 1;
         mainPanel.add(submitButton, constraints);
 
+        // <Chance to Skip Word Slider>
         chanceSlider = new JSlider(JSlider.HORIZONTAL, CHANCE_TO_SKIP_MIN, CHANCE_TO_SKIP_MAX, CHANCE_TO_SKIP_INIT);
         constraints.anchor = GridBagConstraints.CENTER;
         constraints.gridx = 1;
@@ -63,20 +71,20 @@ public class Thesaurusizer {
         chanceSlider.addChangeListener(e -> {
             setChanceToSkip(chanceSlider.getValue());
         });
-
         JLabel sliderLabel = new JLabel("Chance To Skip");
         constraints.gridx = 1;
         constraints.gridy = 1;
         mainPanel.add(sliderLabel);
 
+        // <Clear Result Area Button>
         clearButton = new JButton("Clear");
         constraints.anchor = GridBagConstraints.LINE_END;
         constraints.gridx = 0;
         constraints.gridy = 1;
         mainPanel.add(clearButton, constraints);
 
-        addActionListenersToButtons();
 
+        // <Result Area and Pane>
         resultArea = new JTextArea("");
         constraints.anchor = GridBagConstraints.CENTER;
         resultArea.setLineWrap(true);
@@ -88,10 +96,14 @@ public class Thesaurusizer {
         constraints.gridy = 2;
         mainPanel.add(resultPane, constraints);
 
+        //<Add actions to buttons>
+        addActionListenersToButtons();
+
         frame.validate();
 
         // </Swing>
 
+        //Create thesaurus to pass around
         thesaurusMap = new ThesaurusMap();
     }
 
@@ -131,6 +143,12 @@ public class Thesaurusizer {
         });
     }
 
+    /**
+     * Split a string array into multiple string arrays every x strings.
+     * @param inputArray An array of Strings to be split
+     * @param split The number of strings that will be in each resulting array
+     * @return String[][] The input String array split into multiple smaller arrays based on the split paramerter
+     */
     private String[][] splitStringArray(String[] inputArray, int split){
         String[][] splitWordArrays = new String[inputArray.length / split + 1][];
         String[] splitArray = new String[3];
@@ -149,7 +167,7 @@ public class Thesaurusizer {
         this.chanceToSkip = chanceToSkip;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Thesaurusizer th = new Thesaurusizer();
     }
 }
